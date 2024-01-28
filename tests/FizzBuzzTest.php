27467ea03c0e51b8a8b41c9f50aa29b3b35568f0
ftnext <takuyafjp+develop@gmail.com>
2024-01-28 23:20:32 +0900

    [test] Import FizzBuzz (Red -> Green)

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -1,9 +1,12 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . "/../src/FizzBuzz.php";
+
 class FizzBuzzTest extends TestCase
 {
   public function test_1を渡すと文字列1を返す(): void
   {
+    $fizzBuzz = new FizzBuzz();
   }
 }
